72a22d0691922026657c525dcf575cd1f2aa18db
thiagokrugel <thiagokrugel743@gmail.com>
2022-08-30 08:56:28 -0300

    soninho

--- commands/alcides-uml.js
+++ commands/alcides-uml.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('alcides')
+		.setName('alcides-uml')
 		.setDescription('soninho'),
 	async execute(interaction) {
 		await interaction.reply('https://www.youtube.com/watch?v=x3cZwk9VAzA');
