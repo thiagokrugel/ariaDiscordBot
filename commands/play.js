d392b0af63c57b3587a9699e55a556d1f5efbe61
thiagokrugel <teagumainsombra@gmail.com>
2022-08-31 23:12:56 -0300

    pain

--- commands/play.js
+++ commands/play.js
@@ -1,38 +1,38 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
-const { EmbedBuilder } = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("play")
-		.setDescription("Plays a video from YouTube!")
+		.setDescription("loads songs from youtube")
 		.addSubcommand((subcommand) =>
 			subcommand
 				.setName("song")
-				.setDescription("Plays a video from an URL!")
-				.addStringOption((option) => option.setName("url").setDescription("video's url").setRequired(true))
+				.setDescription("Loads a single song from a url")
+				.addStringOption((option) => option.setName("url").setDescription("the song's url").setRequired(true))
 		)
 		.addSubcommand((subcommand) =>
 			subcommand
 				.setName("playlist")
-				.setDescription("Loads a playlist from an URL!")
-				.addStringOption((option) => option.setName("url").setDescription("playlist's url").setRequired(true))
+				.setDescription("Loads a playlist of songs from a url")
+				.addStringOption((option) => option.setName("url").setDescription("the playlist's url").setRequired(true))
 		)
 		.addSubcommand((subcommand) =>
 			subcommand
 				.setName("search")
-				.setDescription("Plays a video from keywords inputted!")
+				.setDescription("Searches for sogn based on provided keywords")
 				.addStringOption((option) =>
-					option.setName("searchterms").setDescription("keywords").setRequired(true)
+					option.setName("searchterms").setDescription("the search keywords").setRequired(true)
 				)
 		),
 	run: async ({ client, interaction }) => {
-		if (!interaction.member.voice.channel) return interaction.editReply("Join a voice channel to use this!")
+		if (!interaction.member.voice.channel) return interaction.editReply("You need to be in a VC to use this command")
 
 		const queue = await client.player.createQueue(interaction.guild)
 		if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
-		let embed = new EmbedBuilder()
+		let embed = new MessageEmbed()
 
 		if (interaction.options.getSubcommand() === "song") {
             let url = interaction.options.getString("url")
@@ -41,13 +41,12 @@ module.exports = {
                 searchEngine: QueryType.YOUTUBE_VIDEO
             })
             if (result.tracks.length === 0)
-                return interaction.editReply("Aria didn't found anything...")
+                return interaction.editReply("No results")
             
             const song = result.tracks[0]
             await queue.addTrack(song)
             embed
-                .setColor('#c7fabe')
-                .setDescription(`Aria added **[${song.title}](${song.url})** to the queue!\n\n Requested by: <@${song.requestedBy.id}>`)
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Duration: ${song.duration}`})
 
@@ -59,14 +58,13 @@ module.exports = {
             })
 
             if (result.tracks.length === 0)
-                return interaction.editReply("Aria didn't found anything...")
+                return interaction.editReply("No results")
             
             const playlist = result.playlist
             await queue.addTracks(result.tracks)
             embed
-                .setColor('#c7fabe')
-                .setDescription(`Aria added **${result.tracks.length} songs from [${playlist.title}](${playlist.url})** to the queue!\n\n Requested by: <@${song.requestedBy.id}>`)
-                
+                .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** have been added to the Queue`)
+                .setThumbnail(playlist.thumbnail)
 		} else if (interaction.options.getSubcommand() === "search") {
             let url = interaction.options.getString("searchterms")
             const result = await client.player.search(url, {
@@ -75,13 +73,12 @@ module.exports = {
             })
 
             if (result.tracks.length === 0)
-                return interaction.editReply("Aria didn't found anything...")
+                return interaction.editReply("No results")
             
             const song = result.tracks[0]
             await queue.addTrack(song)
             embed
-                .setColor('#c7fabe')
-                .setDescription(`Aria added **[${song.title}](${song.url})** to the queue!\n\n Requested by: <@${song.requestedBy.id}>`)
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Duration: ${song.duration}`})
 		}
